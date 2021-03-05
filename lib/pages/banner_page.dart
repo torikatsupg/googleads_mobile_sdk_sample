0bf69d17a3c1537a2ea0cc4fc5849b933c8d7858
torii katsuya <torikatsu@toriinoMacBook-Pro.local>
2021-03-05 23:54:04 +0900

    add banner ad

--- lib/pages/banner_page.dart
+++ lib/pages/banner_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class BannerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('banner page'),
+      child: Column(
+        children: [
+          Center(
+            child: _buildBannerAd(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBannerAd() {
+    final listener = AdListener(
+      // Called when an ad is successfully received.
+      onAdLoaded: (Ad ad) => print('Ad loaded.'),
+      // Called when an ad request failed.
+      onAdFailedToLoad: (Ad ad, LoadAdError error) {
+        print('Ad failed to load: $error');
+      },
+      // Called when an ad opens an overlay that covers the screen.
+      onAdOpened: (Ad ad) => print('Ad opened.'),
+      // Called when an ad removes an overlay that covers the screen.
+      onAdClosed: (Ad ad) => print('Ad closed.'),
+      // Called when an ad is in the process of leaving the application.
+      onApplicationExit: (Ad ad) => print('Left application.'),
+    );
+
+    final myBanner = BannerAd(
+      size: AdSize.banner,
+      adUnitId: "ca-app-pub-3940256099942544/6300978111",
+      listener: listener,
+      request: AdRequest(),
+    )..load();
+
+    return Container(
+      alignment: Alignment.center,
+      child: AdWidget(ad: myBanner),
+      width: myBanner.size.width.toDouble(),
+      height: myBanner.size.height.toDouble(),
     );
   }
 }
--- lib/run.dart
+++ lib/run.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:googleads_mobile_sdk_sample/app.dart';
 
 run() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
   return runApp(
     ProviderScope(
       child: App(),
